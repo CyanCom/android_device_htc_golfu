23fc89972668f055de7f6b2596e62d0d48305502
Arnav Gosain <nolinuxnoparty@gmail.com>
2013-12-20 19:00:58 +0530

    golfu: update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,18 +16,17 @@
 USE_CAMERA_STUB := true
 BOARD_VENDOR := htc
 
+## cflags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP_WITH_GENLOCK
+COMMON_GLOBAL_CFLAGS += -DMSMFB_METADATA_GET
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
-COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK
-COMMON_GLOBAL_CFLAGS += -DUSE_LEGACY_SCREENSHOT
 COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
-COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
+TARGET_GLOBAL_CFLAGS   += -mtune=cortex-a5 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a5 -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS   += -DQCOM_NO_SECURE_PLAYBACK -DBINDER_COMPAT
 
-# Arch related defines and optimizations
+## Arch related defines and optimizations
 TARGET_BOARD_PLATFORM := msm7x27a
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
@@ -43,31 +42,31 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOOTLOADER_BOARD_NAME := golfu
 TARGET_CORTEX_CACHE_LINE_32 := true
 
-# Compiler Optimization
+## Compiler Optimization
 ARCH_ARM_HIGH_OPTIMIZATION := true
 ARCH_ARM_HIGH_OPTIMIZATION_COMPAT := true
 
-# cortex-a9 is used to take advantage of NEON optimizations
+## cortex-a9 is used to take advantage of NEON optimizations
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_ARCH_VARIANT_FPU := neon
 
-# Optimisations used by Qualcomm
+## Optimisations used by Qualcomm
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
-# Target information
+## Target information
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
-# Kernel
+## Kernel
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x13000000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1200000
 
-# Additional libraries
+## Additional libraries
 TARGET_PROVIDES_LIBAUDIO := true
 BOARD_HAVE_HTC_AUDIO := true
 
-# Fix this up by examining /proc/mtd on a running device
+## Fix this up by examining /proc/mtd on a running device
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 4194304
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8909824
@@ -75,15 +74,15 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 836763136
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1023409664
 BOARD_FLASH_BLOCK_SIZE := 2048
 
-# SDcard support
+## SDcard support
 BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p32
 
-# Inline kernel building
+## Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/htc/golfu
 TARGET_KERNEL_CONFIG := golfu_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
-# Vold
+## Vold
 BOARD_VOLD_MAX_PARTITIONS := 32
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
@@ -92,7 +91,7 @@ BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_otg/msm_hsusb/gadget/lun0/file"
 BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_otg/msm_hsusb/gadget/lun0/file"
 
-# Bluetooth
+## Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/golfu/bluetooth
 TARGET_SPECIFIC_HEADER_PATH := device/htc/golfu/include
@@ -100,27 +99,33 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/golfu/include
 # OpenGL drivers config file path
 BOARD_EGL_CFG := device/htc/golfu/proprietary/lib/egl/egl.cfg
 
-# Wi-Fi
+## Wi-Fi
 BOARD_WLAN_DEVICE := ath6kl
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl_sdio.ko"
-WIFI_DRIVER_MODULE_NAME := "ath6kl_sdio"
+WIFI_EXT_MODULE_NAME := cfg80211
+WIFI_EXT_MODULE_PATH := /system/lib/modules/cfg80211.ko
+WIFI_AP_DRIVER_MODULE_NAME := ath6kl_sdio
+WIFI_AP_DRIVER_MODULE_PATH := /system/lib/modules/ath6kl_sdio.ko
+WIFI_AP_DRIVER_MODULE_ARG := "suspend_mode=3 wow_mode=2 ath6kl_p2p=1 recovery_enable=1"
+WIFI_DRIVER_MODULE_NAME := ath6kl_sdio
+WIFI_DRIVER_MODULE_PATH := /system/lib/modules/ath6kl_sdio.ko
+WIFI_DRIVER_MODULE_ARG := "suspend_mode=3 wow_mode=2 ath6kl_p2p=1 recovery_enable=1"
 
 
-# Misc
+## Misc
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_BOOTANIMATION_USE_RGB565 := true
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
-# Releasetools
+## Releasetools
 TARGET_OTA_EXTRAS_FILE := device/htc/golfu/releasetools/extras.txt
 
-# GPS
+## GPS
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USES_QCOM_GPS := true
 BOARD_USES_QCOM_HARDWARE := true
@@ -128,7 +133,7 @@ BOARD_USES_QCOM_LIBS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := golfu
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
-# Graphics & CAF
+## Graphics & CAF
 COMMON_GLOBAL_CFLAGS += -DANCIENT_GL
 BOARD_USE_SKIA_LCDTEXT := true
 USE_OPENGL_RENDERER := true
@@ -140,11 +145,11 @@ TARGET_QCOM_MEDIA_VARIANT := legacy
 BOARD_EGL_NEEDS_LEGACY_FB := true
 TARGET_USES_PMEM := true
 TARGET_GRALLOC_USES_ASHMEM := true
-TARGET_USES_C2D_COMPOSITION := false
+TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_SF_BYPASS := false
 TARGET_HAVE_BYPASS := false
 TARGET_USES_GENLOCK := true
-TARGET_USES_OVERLAY := true
+TARGET_USES_OVERLAY := false
 BOARD_USE_QCOM_LLVM_CLANG_RS := true
 USE_OPENGL_RENDERER := true
 BOARD_EGL_NEEDS_LEGACY_FB := true
@@ -152,21 +157,21 @@ TARGET_NO_HW_VSYNC := false
 TARGET_QCOM_HDMI_OUT := false
 #TARGET_LIBAGL_USE_GRALLOC_COPYBITS := true
 #BOARD_AVOID_DRAW_TEXTURE_EXTENSION := true
-#TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
+TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
 
-# ICS Stuff
+## ICS Stuff
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-# RIL
+## RIL
 BOARD_USE_NEW_LIBRIL_HTC := true
 BOARD_USES_LEGACY_RIL := true
 
-# Camera
+## Camera
 BOARD_NEEDS_MEMORYHEAPPMEM := true
 CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
 BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
 
-# Add h/w acceleration in browser
+## Add h/w acceleration in browser
 ENABLE_WEBGL := true
 WITH_JIT := true
 ENABLE_JSC_JIT := true
@@ -174,31 +179,31 @@ JS_ENGINE := v8
 HTTP := chrome
 TARGET_FORCE_CPU_UPLOAD := true
 
-# Touch screen compatibility for JB
+## Touch screen compatibility for JB
 BOARD_USE_LEGACY_TOUCHSCREEN := true
 
-# Custom liblights
+## Custom liblights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Keep Size in check
+## Keep Size in check
 SMALLER_FONT_FOOTPRINT := true
 
-# Power
+## Power
 TARGET_PROVIDES_POWERHAL := true
 
-# Recovery
+## Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := device/htc/golfu/fstab.golfu
 RECOVERY_FSTAB_VERSION := 2
 
-# TWRP
+## TWRP
 DEVICE_RESOLUTION := 320x480
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_NEVER_UMOUNT_SYSTEM := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 
 
-# FM Radio
+## FM Radio
 BOARD_HAVE_FM_RADIO := true
 BOARD_FM_DEVICE := bcm4330
 BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
